954b819a8821ae51edfe5b07fdf0a0b203cafe75
xiegonghai <312182515@qq.com>
2016-06-25 16:16:07 +0800

    rotate image

---
+++ 48-rotate image.cpp
@@ -0,0 +1,12 @@
+class Solution{
+  public:
+    void rotate(vector<vector<int>>&matrix){
+        const int n=matrix.size();
+        for(int i=0;i<n;++i)//先副对角线对折
+            for(int j=0;j<n-i;++j)
+                swap(matrix[i][j],matrix[n-1-j][n-1-i]);
+        for(int i=0;i<n/2;++i)//沿水平中线对折
+            for(int j=0;j<n;++j)
+                swap(matrix[i][j],matrix[n-1-i][j]);
+    }
+};
